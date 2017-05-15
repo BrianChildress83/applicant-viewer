e4a3476d6800305fd4d0b65728cb696c5b589351
Brian Childress <brian@brianchildress.co>
2017-05-14 23:30:52 -0400

    Adding providers, applicant module, CommonModule, HttpModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
+import { AppProviders } from './app.providers';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { ApplicantModule } from './applicant/applicant.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { AppComponent } from './app.component';
@@ -13,12 +17,15 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
+        CommonModule,
         CoreModule,
+        HttpModule,
         SharedModule,
+        ApplicantModule,
         HomeModule,
         AppRoutingModule
     ],
+    providers: [ AppProviders ],
     bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule {}
