ba5e3c7771dd8fccdb2122f7b5f6693969abcab3
Brian Childress <brian@brianchildress.co>
2017-05-15 23:56:23 -0400

    Updating main app module to remove unused modules, updating formatting.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,30 +7,26 @@ import { HttpModule } from '@angular/http';
 import { AppProviders } from './app.providers';
 import { ComponentsModule } from './components/components.module';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { ApplicantModule } from './applicant/applicant.module';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeModule } from './home/home.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        CommonModule,
-        ComponentsModule,
-        CoreModule,
-        FormsModule,
-        HttpModule,
-        ReactiveFormsModule,
-        SharedModule,
-        ApplicantModule,
-        HomeModule,
-        AppRoutingModule
-    ],
-    providers: [ AppProviders ],
-    bootstrap: [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    ComponentsModule,
+    CoreModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule,
+    ApplicantModule,
+    AppRoutingModule
+  ],
+  providers: [AppProviders],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
