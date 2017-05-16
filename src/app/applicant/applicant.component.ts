0d439c47c98813312c3eb35309a570a09f155dd9
Brian Childress <brian@brianchildress.co>
2017-05-15 23:57:34 -0400

    Updating main applicant component to utilize new datatable component. Interaction with applicant and favorite service

--- src/app/applicant/applicant.component.ts
+++ src/app/applicant/applicant.component.ts
@@ -1,26 +1,57 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-import {
-  ApplicantService
-} from '../services/applicant.service';
+import { ApplicantService } from '../services/applicant.service';
 
+/**
+ * ApplicantComponent -  Used to display and interacte with all applicants
+ * information
+ * @requires Component, OnInit
+ * @requires Router
+ * @requires ApplicantService
+ */
 @Component({
   selector: 'app-applicant',
   templateUrl: './applicant.component.html'
 })
 
 export class ApplicantComponent implements OnInit {
-  applicantsArray: any;
-  constructor(private applicantSvc: ApplicantService) {}
 
+  applicantsArray: any;
+
+  columns: Array < Object > = [{
+    value: 'name',
+    header: 'Name'
+  }, {
+    value: 'position',
+    header: 'Position'
+  }, {
+    value: 'experience',
+    header: 'Experience'
+  }];
+
+  constructor(private router: Router,
+    private applicantSvc: ApplicantService) {}
+
+  /**
+   * ngOnInit - Initializes with component
+   */
   ngOnInit() {
+
     this.applicantSvc.getAllApplicants().subscribe(
       applicants => {
-        console.log(applicants);
         this.applicantsArray = applicants;
       });
+
+  }
+
+  /**
+   * setSelectedRow - Set a selected row based on what is returned from datatable component
+   * @param { Object } evt The event returned from the datatable event emitter
+   */
+  setSelectedRow(evt) {
+
+    this.router.navigate(['/applicant', evt.id]);
+
   }
 }
